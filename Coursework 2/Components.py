e913e0f50ca50598e3489f729a39d77626c42559
msnshorenye <ms1727@exeter.ac.uk>
2025-12-14 02:22:56 +0000

    Update

--- Coursework 2/Components.py
+++ Coursework 2/Components.py
@@ -6,27 +6,27 @@ def initialise_board(size = 8):
     Takes the size of the board desired  It creates an empty 2d array of that size and fills the middle spaces with Light and Dark and the rest with None.
     It then returns this now set up game board
     """
-    board_list = [["","","","","","","",""],["","","","","","","","",],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""]]
-    for i in range(0,size):
-        for x in range(0,size):
+    board_list = [["","","","","","","",""],["","","","","","","","",],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""],["","","","","","","",""]] #8 by 8 2d array
+    for i in range(0,size): #for loop for all x coordinates
+        for x in range(0,size): #for loop for all y coordinates in the 2d array
     
 
-            if  i == 3 and x == 3:
-                board_list[i][x] = "Light  "
+            if  i == 3 and x == 3: # checks for coordinate (3, 3)
+                board_list[i][x] = "Light  " #Makes it equal to the string light representing the light counter
             
-            elif i == 3 and x == 4:
-                board_list[i][x] = "Dark   "
+            elif i == 3 and x == 4: #checks for coorrdinate()
+                board_list[i][x] = "Dark   " 
             
             elif i ==4 and x == 3:
                 board_list[i][x] = "Dark   "
             elif i == 4 and x == 4:
-                board_list[i] [x] = "Light  "
-            else:
-                board_list[i][x] =  "None   "
+                board_list[i] [x] = "Light  " 
+            else: #if it is not one of the specific centre coordinates at the start of the game then it should not be played
+                board_list[i][x] =  "None   " #every other coordinate should be set to empty
 
           
     
-    return(board_list)
+    return(board_list) #returns the gam board 2d list so that it can be used and changed in the othello game
     
 def print_board(board):
     """
@@ -35,8 +35,8 @@ def print_board(board):
     :param board: The gameboard which is a 2d array that represents the current game board  state in Ascii
     It iterates through each index of the board and prints them out to make it easy to see where counters are and what colour each square of the board is 
     """
-    for i in range(0, len(board)):
-        print(board[i]) 
+    for i in range(0, len(board)): #iterates through the 8 rows in the board
+        print(board[i]) #prints out each row
 
    
 def switch_colour(colour):
@@ -46,12 +46,12 @@ def switch_colour(colour):
     :param colour: The colour as a string of the player who last played
     It takes the colour of the player and uses an if statement to return the other players colour
     """
-    if colour == "Light":
-        return("Dark")
-    else:
-        return("Light")
+    if colour == "Light": #selection if the colour is the light player it should return the next players colour Light
+        return("Dark") 
+    else: #If the player's colour given is not light the next colour should be light
+        return("Light") 
 
-def legal_move(colour, coordinate, board, size = 8):
+def legal_move(colour, coordinate, board, size = 8): 
     """
     Docstring for legal_move
     
@@ -62,15 +62,12 @@ def legal_move(colour, coordinate, board, size = 8):
     It checks whether the move is legal and then flips the counters in the directions which their is an opposition counter and then one of the players own colours.
     It checks this for every direction possible 
     """
-    legal_turn = False 
-    y = int(coordinate[0])
+    legal_turn = False #Legal turn is set to false intially as a legal move has not been found yet
+    y = int(coordinate[0]) #Takes the list inputted by the player 
+    #and switchest the index around to get x and y as the first indexx as a 3d 
     x = int(coordinate[1])
-    
-    print(x)
-    print(y)
-    print(board[x])
-    print(board[x][y])
-    if colour == "Light":
+
+    if colour == "Light": #same logic as switch colour within the legal move function 
         opposite_colour = "Dark   "
         actual_colour = "Light  "
     elif colour == "Dark":
@@ -78,36 +75,35 @@ def legal_move(colour, coordinate, board, size = 8):
         actual_colour = "Dark   "
 
     
-    if board[x][y] == "None   ":
+    if board[x][y] == "None   ": # For a move to be made the position has to be none otherwise it already has an assigned counter
 
-        if x+1 <= 7 and board[x+1][y] == opposite_colour:
+        if x+1 <= 7 and board[x+1][y] == opposite_colour: #all adjacent directions coordinates within the size of the board are checked with selection
+        #and that direction is on the board
+        # if they are equal to opposite_colour 
+        # then the move can be checked whether there is a playable move
 
-                y_distance = 0
-                x_distance = 1 
-                a = x+1
+                y_distance = 0  #an x and y distance are set to the direction of the coordinates being checked
+                x_distance = 1 #checking to the right in the same row so 0 and 1 are the values
+                a = x+1 #a and b are the values of the coordinates being added ib
                 b = y
-                to_flip = []
-                to_flip.append((a, b))
+                to_flip = [] #list created so that coordinates found that should be flipped can be saved 
+                to_flip.append((a, b)) #these coordinates which are the opposite of the players are the first appe
                 while 0 <= a  <= 7 and  0<= b <= 7 and board[a][b] == opposite_colour :
-                    a = a + x_distance
+                    #While the coordinates are in the bounds of the board and the counters are opposite colour the code should loop
+                    a = a + x_distance #we add x distance and y distance to the coordinate
+                    #so that we get the next counter in that direction
                     b = b + y_distance
-                    to_flip.append((a, b))
-                    if 0 <= a  <= 7 and  0<= b <= 7:
-                        if board[a][b] == actual_colour:
-                            for (i, j) in to_flip: 
-                                board[i][j] = actual_colour
-                                legal_turn = True
+                    to_flip.append((a, b)) #if it is equal to opposite colour the new coordinate 
+                    if 0 <= a  <= 7 and  0<= b <= 7: #checks that the new coordinate is also within the boards coordinates
+                        if board[a][b] == actual_colour: #if the next coordinate is equal to the players colour it means we can stop flipping coordinates
+                            for (i, j) in to_flip: #for each tuple coordinate in the list
+                                board[i][j] = actual_colour #the coordinates of the tuple 
+                                legal_turn = True #legal turn can be set to true as a move has been made
                             board[x][y] = actual_colour
+                            #while loop will stop as board[a][b]
 
-                # for i in range(x+1, size):
-                #     if board[i][y] == actual_colour:
-                #         for z in range(x, i):
-                #             board[z][y] = actual_colour
-                #         board[x][y] = actual_colour
-                            
-                #         legal_turn = True
                 
-        if x-1 >= 0 and board[x-1][y] == opposite_colour:
+        if x-1 >= 0 and board[x-1][y] == opposite_colour: #same logic for each direction
 
             
             y_distance = 0
@@ -127,16 +123,7 @@ def legal_move(colour, coordinate, board, size = 8):
                                 board[i][j] = actual_colour
                                 legal_turn = True
                             board[x][y] = actual_colour
-            # for row in range(0, x):
-            #     print(board[row][y])
-            #     if board[row][y] == actual_colour:
-            #         print('This is for the backwards row')
-            #         print(board[row][y])
-            #         print(opposite_colour)
-            #         for found_row in range(row, x):
-            #             board[found_row][y] = actual_colour
-            #             legal_turn = True
-            #         board[x][y] = actual_colour
+
 
         if y+1 <= 7 and board[x][y+1] == opposite_colour :
                 
@@ -158,15 +145,7 @@ def legal_move(colour, coordinate, board, size = 8):
                                 legal_turn = True
                             board[x][y] = actual_colour
                     
-                # for column in range(y+1, size):
-                #     if board[x][column] == opposite_colour:
-                #         print(column)
-                #         print()
-                #         for found_column in range(y, column+1):
-                #             print(found_column)
-                #             board[x][found_column] = actual_colour
-                #             legal_turn = True
-                #         board[x][y] = actual_colour
+
         
         if  y-1 >= 0 and board[x][y-1] == opposite_colour:
 
@@ -187,16 +166,6 @@ def legal_move(colour, coordinate, board, size = 8):
                             board[i][j] = actual_colour
                             legal_turn = True
                         board[x][y] = actual_colour
-            # for back_column in range(0, y):
-            #     print(back_column)
-            #     print(board[x][back_column])
-            #     if board[x][back_column] == opposite_colour:
-            #         for found_back_column in range(back_column, y):
-            #             print('We have found oppos')
-            #             board[x][found_back_column] = actual_colour
-            #             legal_turn = True
-
-            #         board[x][y] = actual_colour
         
         if x+1<=7 and y+1 <=7 and board[x+1][y+1] == opposite_colour:
                 y_distance = 1
@@ -239,15 +208,6 @@ def legal_move(colour, coordinate, board, size = 8):
                     
                         board[x][y]  = actual_colour
                         legal_turn = True
-            # # for diag_row_back in range(0,x-1):
-            # #     for diag_column_back in range(0, y-1):
-            # #         if board[diag_row_back][diag_column_back] == actual_colour:
-            # #             for found_diag_row_back in range(diag_row_back, x):
-            # #                 for found_diag_column_back in range(diag_column_back, y):
-            # #                     board[found_diag_row_back][found_diag_column_back] = actual_colour
-            # #                     legal_turn = True
-            # #             board[x][y] = actual_colour
-
 
         if x+1 <=7 and y-1 >= 0 and board[x+1][y-1] == opposite_colour :
                 
@@ -274,14 +234,6 @@ def legal_move(colour, coordinate, board, size = 8):
                             legal_turn = True
                 
                 
-                # for diag_row_front in range(x+1,size):
-                #     for diag_column_back2 in range(0, y-1):
-                #         if board[diag_row_front][diag_column_back2] == actual_colour:
-                #             for found_diag_row_front in range(x, diag_row_front):
-                #                 for found_diag_column_back2 in range(diag_column_back2, y):
-                #                     board[found_diag_row_front][found_diag_column_back2] = actual_colour
-                #                     legal_turn = True
-                #             board[x][y] = actual_colour
         
         if x-1>= 0 and y+1 <= 7 and board[x-1][y+1] == opposite_colour:
                 
@@ -308,18 +260,10 @@ def legal_move(colour, coordinate, board, size = 8):
                             board[x][y]  = actual_colour
                             legal_turn = True
                 
-                # for diag_row_back2 in range(0,x-1):
-                #     for diag_column_up in range(y+1, size):
-                #         if board[diag_row_back2] [diag_column_up] == actual_colour:
-                #             for found_diag_row_back in range(diag_row_back2, x):
-                #                 for found_diag_column_up in range(y, diag_column_up):
-                #                     board[found_diag_row_back][found_diag_column_up] = actual_colour
-                #                     legal_turn = True
-                #             board[x][y] = actual_colour
         
 
         print(legal_turn)
-        return(legal_turn)
+        return(legal_turn) #return the boolean value whether
 
 
 
--- Coursework 2/flask_server_ai_player.py
+++ Coursework 2/flask_server_ai_player.py
@@ -9,73 +9,83 @@ from game_engine import check_win
 from game_engine import check_if_any_possible_move
 from game_engine import ai_player_move
 from game_engine import check_board_full
-
-app = Flask(__name__)
-Current_Player = "Dark"
+import logging
+from game_engine import main
+app = Flask(__name__) # making a flask server called app
+Current_Player = "Dark" #starting player should be set to Dark
 Light = "Light"
-move_counter = 60
-g_b_2 = initialise_board()
-
+move_counter = 60 #set up the maxium move count to 60
+g_b_2 = initialise_board() #make the board to render the template
+logger = logging.getLogger(__name__)
 @app.route('/')
-@app.route('/index', methods = ['GET','POST'])
+@app.route('/index', methods = ['GET','POST']) 
 def index():
     """
-    Makes the template
+    Makes the template using render template by intialising a game board and then using it as board with the html template 
+    This function is called when the template and the flask framework web server is loaded. 
     """
-    g_b  = initialise_board()
-    return render_template('Othello Board.html',game_board = g_b)
+    g_b  = initialise_board() #make the board
+    return render_template('Othello Board.html',game_board = g_b) #makes the template when the web server is opened up
 @app.route('/move', methods = ['GET', 'POST'])
 
 def get_move():
    """
    Docstring for get_move
+
    """
-   if request.method == 'GET':
-      global Current_Player
-      global move_counter
+   if request.method == 'GET': #if the re
+      global Current_Player #making current player global so that It can be used in the get move function
+      global move_counter #making move counter global so that it can be decreased in the get move function 
       print('This is the game board')
-      print_board(g_b_2)
+      print_board(g_b_2) 
 
       #tmp = switch_colour(current_player)
       #current_player = tmp
-      x = request.args.get('x', type=int)
+      x = request.args.get('x', type=int) #getting the x coordinate that has been clicked by the player
       print(x)
-      y = request.args.get('y', type=int)
+      y = request.args.get('y', type=int) #getting the y coordinate that has been clicked by the player
       print(y)
-      coord_list = [x-1, y-1]
-      any_moves = check_if_any_possible_move(g_b_2, Current_Player)
-      board_full = check_board_full(g_b_2)
+      coord_list = [x-1, y-1] #zero indexing means the coordinates
+      logging.info(F"Dark coordinates played {x} and {y}")
+      any_moves = check_if_any_possible_move(g_b_2, Current_Player) #checks if their are any possible moves to be made by the current playerr
+      board_full = check_board_full(g_b_2) #checking if the board is full so that a move can be played
       print('This is the value for the any possible moves check', any_moves)
-      if move_counter > 0 and any_moves is True and board_full is True:
-         move = legal_move(Current_Player,coord_list,g_b_2)
-         any_moves = check_if_any_possible_move(g_b_2, Light)
-         if any_moves is True:
+      if move_counter > 0 and any_moves is True and board_full is True: #if all the game conditions 
+         move = legal_move(Current_Player,coord_list,g_b_2) 
+         any_moves = check_if_any_possible_move(g_b_2, Light) #function call to make sure the ai can play a move on the board
+         if any_moves is True: #if there is an available 
             ai_move = ai_player_move(g_b_2)
             if move is True and ai_move is True:
                return{'status': 'success', 'player' : switch_colour(Current_Player), 'board': g_b_2}
             
-            if any_moves is True and ai_move is False:
+            if any_moves is True and ai_move is False: #If the ai cant make a move but their is an available move their should be a turn played
                return{'status': 'success', 'player' : switch_colour(Current_Player), 'board': g_b_2}
             else:
                return{'status': 'fail', 'message': Current_Player}
          else:
-             return{'finished': check_win(g_b_2), 'board': g_b_2}
+             return{'finished': check_win(g_b_2), 'board': g_b_2} 
       else:
-          return{'finished': check_win(g_b_2), 'board': g_b_2} 
+          return{'finished': check_win(g_b_2), 'board': g_b_2}  #if their are no more moves allowed or avaliable or the board is full
+         #the game is finished so it returns the board and winner to the html template
 @app.route('/save', methods= ['GET', 'POST'])
 def save_board():
    """
    Docstring for save_board
+   returns the current gameboard on click of the html save button
    """
    if request.method == 'GET':
-      return{'board': g_b_2}
+      return{'board': g_b_2} #returns the game board so that it can be saved in a json Stringfy data type
 @app.route('/load', methods = ['GET', 'POST'])
 def load_board():
    """
    Docstring for load_board
+   Gets data as text of the saved game boardon click of the load button 
+   and then uses Json loads  
+   to format the gameboard back into a 2d list 
+   so that it can be used in the render template function. 
    """
    if request.method == 'POST':
-      b = request.get_data(as_text = True)
+      b = request.get_data(as_text = True) #gets the game board
       b_2 = json.loads(b)
       return render_template('Othello Board.html', game_board = b_2)
 if __name__ == '__main__':
